e1de691703689d744489a4dbfeadc8deadcc46a3
ajhridoy <ajhridoy461@gmail.com>
2022-11-24 04:07:20 +0600

    Added responsive navber and banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import laptop from '../../../assets/best-chromebook-laptops-1660750023.jpg'
+import laptop2 from '../../../assets/photo-1519389950473-47ba0277781c.jpg'
+import laptop3 from '../../../assets/Best-Laptop-Brands-in-World-1.png'
+
+const Banner = () => {
+    return (
+        <div className="hero md:h-[500px]" style={{ backgroundImage: `url(${laptop3})`, backgroundRepeat: 'no-repeat'}}>
+  <div className="hero-overlay bg-opacity-80"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-sm md:max-w-lg">
+      <h1 className="mb-5 text-3xl md:text-6xl text-yellow-400 font-bold">Buy or Sell Your Laptop</h1>
+      <p className="mb-5 md:text-lg text-green-400 font-medium">If you want to buy a second hand laptop or sell your used laptop at a good price then this website is just for you. We provide the best deals to both sellers and buyers.</p>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Categoris/Categoris.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categoris = () => {
+    return (
+        <div className='my-10'>
+            <h2 className='text-4xl text-center font-bold mb-5'>Categories</h2>
+        </div>
+    );
+};
+
+export default Categoris;
