fac7d8d0dd36c5c75c6cb9810a9e945bffba44fa
ajhridoy <ajhridoy461@gmail.com>
2022-11-24 05:17:46 +0600

    added categories section

--- src/Pages/Home/Categoris/Categoris.js
+++ src/Pages/Home/Categoris/Categoris.js
@@ -1,9 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Category from './Category';
 
 const Categoris = () => {
+    const {data: categories = []} = useQuery({
+        queryKey: ['category'],
+        queryFn: () => fetch('category.json')
+        .then(res => res.json())
+    })
     return (
         <div className='my-10'>
-            <h2 className='text-4xl text-center font-bold mb-5'>Categories</h2>
+            <h2 className='text-4xl text-center font-bold mb-10'>Categories</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:mx-14 p-4'>
+                {
+                    categories.map(category => <Category
+                        key={category._id}
+                        category={category}
+                    ></Category>)
+                }
+            </div>
         </div>
     );
 };
